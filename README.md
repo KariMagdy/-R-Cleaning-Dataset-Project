57ebd54f7ca947add5a5b65a42a8d1b85f5dde6e
Karim M. Ibrahim <Karim.m.ibraheem@gmail.com>
2017-05-15 19:48:06 +0800

    Adding dataset

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # -R-Cleaning-Dataset-Project
+
+The script run_analysis.R performs the following: 
+	- Reading the Dataset from UCI HAR Dataset
+	- combining the training and testing dataset
+	- Renaming the data columns appropriately
+	- Summarizing the data by averaging the sensors measurments for each subject and each activity 
--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,18 @@
 library(dplyr)
 
 # Getting Feature Names
-FeaturesNames <- read.table("features.txt",header = F, stringsAsFactors = F)[,2]
+FeaturesNames <- read.table("UCI HAR Dataset/features.txt",header = F, stringsAsFactors = F)[,2]
 
 #Reading Test Dataset
-x_test <- read.table(textConnection(gsub("[ \t]+", " ", readLines("test/X_test.txt"))),header = F, col.names = FeaturesNames)
-y_test <- read.csv("test/y_test.txt",header = F, col.names = "Activity")
-subject_id_test <- read.csv("test/subject_test.txt",header = F, col.names = "Subject_ID")
+x_test <- read.table(textConnection(gsub("[ \t]+", " ", readLines("UCI HAR Dataset/test/X_test.txt"))),header = F, col.names = FeaturesNames)
+y_test <- read.csv("UCI HAR Dataset/test/y_test.txt",header = F, col.names = "Activity")
+subject_id_test <- read.csv("UCI HAR Dataset/test/subject_test.txt",header = F, col.names = "Subject_ID")
 TestDataset <- cbind(subject_id_test,x_test,y_test)
 
 #Reading Train Dataset
-x_train <- read.table(textConnection(gsub("[ \t]+", " ", readLines("train/X_train.txt"))),header = F, col.names = FeaturesNames)
-y_train <- read.csv("train/y_train.txt",header = F, col.names = "Activity")
-subject_id_train <- read.csv("train/subject_train.txt",header = F, col.names = "Subject_ID")
+x_train <- read.table(textConnection(gsub("[ \t]+", " ", readLines("UCI HAR Dataset/train/X_train.txt"))),header = F, col.names = FeaturesNames)
+y_train <- read.csv("UCI HAR Dataset/train/y_train.txt",header = F, col.names = "Activity")
+subject_id_train <- read.csv("UCI HAR Dataset/train/subject_train.txt",header = F, col.names = "Subject_ID")
 TrainDataset <- cbind(subject_id_train,x_train,y_train)
 
 
